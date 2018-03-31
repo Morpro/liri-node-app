75520317dffea3f1f04e26f841222d0b61219f9d
Yamil Morales <yamilmorales@Yamils-MacBook-Pro.local>
2018-03-31 11:39:55 -0400

    FINAL DONE

---
+++ liri-master/keys.js
@@ -0,0 +1,13 @@
+// console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'xJ4Dh9ufuSClB1oPRY58VKewY',
+  consumer_secret: 'wWl6y1HfV024QZGxVErXP92adqHGsrDzeFP5pugQ5wCDuK2ttZ',
+  access_token_key: '979401613252136960-37FdD0S6tuxhcCiDARJlI74GPOr4GE6',
+  access_token_secret: 'bSyVHu3nOBiSgI0aTfWftozZKEtcwodOe3UOOD9mqtrkc',
+}
+
+exports.spotifyKeys = {
+	id: '5cd1d172f98d42cd9296df65e920a800',
+	secret: '13d2cf2f85d04119bdebc354afceef57',
+}
